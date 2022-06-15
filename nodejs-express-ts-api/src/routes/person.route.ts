60bef029fd64b8b2bfc6b43a12d8795fdbfbdcef
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-06-15 09:46:05 -0300

    nodejs-express-ts-api: Comentário TODO teste com ExpressRouter

--- nodejs-express-ts-api/src/routes/person.route.ts
+++ nodejs-express-ts-api/src/routes/person.route.ts
@@ -7,6 +7,8 @@ const PersonRouter = express.Router();
 const personCtl = new PersonController();
 PersonRouter.get("/:id", personCtl.getById.bind(personCtl));
 PersonRouter.post("/search", personCtl.get.bind(personCtl));
+// TODO: testar se essa forma abaixo funciona para substituir as duas linhas subsequentes
+// PersonRouter.post("/", personCtl.post.bind(personCtl)).put("/", personCtl.put.bind(personCtl));
 PersonRouter.post("/", personCtl.post.bind(personCtl));
 PersonRouter.put("/", personCtl.put.bind(personCtl));
 PersonRouter.delete("/:id", personCtl.delete.bind(personCtl));
