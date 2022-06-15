8ce52b16c021d0d685a56440e26af602dbca8abc
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-06-15 09:48:36 -0300

    nodejs: Events

--- nodejs/ui/events_run.js
+++ nodejs/ui/events_run.js
@@ -6,7 +6,26 @@ const emitter = Events.createEmitter(`learning-events`);
 emitter.on(`learning-events`, function () {
   console.log(`learning-events happened! ${new Date()}`.yellow);
 });
-emitter.on(`outro`, function () {
-  console.log(`outro! ${new Date()}`.yellow);
-});
-Events.triggerEvent(emitter, `outro`);
+emitter.on(`outro`, logAll);
+emitter.on(`outro`, logUntil3);
+emitter.once(`outro`, logOnce);
+let count = 0;
+setInterval(() => {
+  count++;
+  Events.triggerEvent(emitter, `outro`, count);
+}, 1000);
+
+console.log(Events.getEventNames(emitter));
+
+function logAll(c) {
+  console.log(`outro! ${new Date()}: ${c}`.yellow);
+}
+function logUntil3(c) {
+  console.log(`Logará até a terceira! ${new Date()}: ${c}`.magenta);
+  if (c >= 3) {
+    Events.turnOffEvent(emitter, `outro`, logUntil3);
+  }
+}
+function logOnce(c) {
+  console.log(`Com emitter.once, é só uma vez! ${new Date()}: ${c}`.green);
+}
