061758321027f7b2952adeaf2c93852ca6c336ca
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-08-05 14:37:38 -0300

    python-fastapi: Copy from Gitlab.com

---
+++ python-fastapi/model/user.py
@@ -0,0 +1,9 @@
+from pydantic.main import BaseModel
+from typing import Optional
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
