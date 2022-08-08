b27a20bd096bbbd5dc9a143e3f64a53a1129b141
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-08-08 12:30:04 -0300

    angular13.web3ui-ethersjs: mudança no componente erc20-transfer-event para receber a lista de eventos via @Input() ao invés de interagir com o contrato

--- angular13/src/app/web3-ui/ethersjs/components/erc20-transfer-event/erc20-transfer-event.component.ts
+++ angular13/src/app/web3-ui/ethersjs/components/erc20-transfer-event/erc20-transfer-event.component.ts
@@ -1,26 +1,15 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ethers, Signer } from 'ethers';
-import { Result } from 'ethers/lib/utils';
+import { Component, Input } from '@angular/core';
 import { ITableColumn } from 'src/app/shared/components/table/tableColumn.interface';
 
-import { EventData } from 'web3-eth-contract';
-import { Web3Subscription } from '../../../shared/model/events/Subscription';
-import { LINK_TOKEN_ABI } from '../../ABI';
-import { ERC20BaseContract } from '../../services/ERC20-base';
-import { EthersjsService } from '../../services/ethersjs.service';
-import { ITransferEvent } from './transfer-event.interface';
-
-declare let window: any;
+import { ITransferEvent } from '../../../shared/model/interfaces/transfer-event.interface';
 
 @Component({
   selector: 'dapp-erc20-transfer-event',
   templateUrl: './erc20-transfer-event.component.html',
   styleUrls: ['./erc20-transfer-event.component.css'],
 })
-export class ERC20TransferEventComponent implements OnInit {
-  @Input() contractERC20!: ERC20BaseContract;
-
-  eventList: ITransferEvent[] = [];
+export class ERC20TransferEventComponent {
+  @Input() eventList: ITransferEvent[] = [];
 
   displayedColumns: ITableColumn[] = [
     { propertyName: 'blockNumber', headerTitle: '# Block' },
@@ -28,96 +17,4 @@ export class ERC20TransferEventComponent implements OnInit {
     { propertyName: 'to', headerTitle: 'To Account' },
     { propertyName: 'value', headerTitle: '$' },
   ];
-
-  constructor(private _ethersjsService: EthersjsService) {}
-
-  jureba: any;
-
-  async ngOnInit(): Promise<void> {
-    this._ethersjsService.getSignerSubject().subscribe(async (_signer) => {
-      //Se a conta não for nula, cria uma nova subscrição filtrando por eventos `Transfer`
-      // que tenha a conta `from` igual à conta conectada na Wallet
-      if (_signer) {
-        // subscrição eventos últimos 1000 blocos
-        this.fetchPastTransferEvents(_signer);
-        // subscrição eventos futuros
-        await this.contractERC20.subscribeContractEvent({
-          eventName: 'Transfer',
-          args: [await _signer.getAddress()],
-          listenerFunction: (from, to, value, event) => {
-            this.eventList = [
-              ...this.eventList,
-              {
-                blockNumber: event.blockNumber,
-                from,
-                to,
-                value,
-              },
-            ];
-          },
-        });
-      }
-    });
-  }
-
-  /**
-   * Fetches the past events on the blockchain since current block less 1000 and feed the {pastEvents} array
-   *
-   * @param signer Account address used to filter the events where 'from' part equals it
-   */
-  private async fetchPastTransferEvents(_signer: Signer): Promise<void> {
-    const currentBlock = await this._ethersjsService.getCurrentBlockNumber();
-    // subscrição eventos passados
-    const pastEvents = await this.contractERC20.getContractsPastEvent({
-      eventName: 'Transfer',
-      args: [],
-      fromBlock: currentBlock - 1000,
-      toBlock: 'latest',
-    });
-
-    ////////////////////////////////
-
-    // const linkContract = new ethers.Contract(
-    //   '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
-    //   LINK_TOKEN_ABI,
-    //   new ethers.providers.Web3Provider(window.ethereum)
-    // );
-    // const filterTransfer = linkContract.filters['Transfer']();
-    // const transferEvents = await linkContract.queryFilter(
-    //   filterTransfer,
-    //   currentBlock - 50,
-    //   'latest'
-    // );
-    // console.log(
-    //   'Transfers____',
-    //   transferEvents.map((e) => {
-    //     return { transaction: e.transactionHash, block: e.blockNumber };
-    //   }),
-    //   transferEvents
-    // );
-
-    // console.warn(
-    //   '#DEBUG',
-    //   transferEvents.length,
-    //   currentBlock,
-    //   transferEvents[transferEvents.length - 1],
-    //   transferEvents[0],
-    //   //      33084061, // BLOCK WHERE IS THE TRANSACTION IN KOVAN
-    //   11138995, // BLOCK WHERE IS THE TRANSACTION IN RINKEBY
-    //   pastEvents.filter((e) => {
-    //     return e.transactionHash.startsWith('0x55c2dd6b760a');
-    //   })
-    // );
-
-    const tempArray = [];
-    for (const e of pastEvents) {
-      tempArray.push({
-        blockNumber: e.blockNumber,
-        from: (<Result>e.args)['from'],
-        to: (<Result>e.args)['to'],
-        value: (<Result>e.args)['value'],
-      });
-    }
-    this.eventList = [...this.eventList, ...tempArray];
-  }
 }
--- angular13/src/app/web3-ui/ethersjs/components/erc20-transfer/erc20-transfer.component.ts
+++ angular13/src/app/web3-ui/ethersjs/components/erc20-transfer/erc20-transfer.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Signer } from 'ethers';
+import { Result } from 'ethers/lib/utils';
 import { BaseFormComponent } from 'src/app/shared/pages/base-form/base-form.component';
 import { MessageService } from 'src/app/shared/services/message.service';
 import { NumbersService } from 'src/app/shared/services/numbers.service';
+import { ITransferEvent } from 'src/app/web3-ui/shared/model/interfaces';
 import { ethereumAddressValidator } from 'src/app/web3-ui/shared/validators/ethereumAddress.validator';
 import { TransactionResult } from '../../../shared/model';
 import { ERC20BaseContract } from '../../services/ERC20-base';
+import { EthersjsService } from '../../services/ethersjs.service';
 
 @Component({
   selector: 'dapp-erc20-transfer',
@@ -21,10 +25,12 @@ export class ERC20TransferComponent
   @Input() decimals: number = 1;
 
   isLoading = false;
+  eventList: ITransferEvent[] = [];
 
   constructor(
     private _formBuilder: FormBuilder,
-    private _messageService: MessageService
+    private _messageService: MessageService,
+    private _ethersjsService: EthersjsService
   ) {
     super();
   }
@@ -41,6 +47,32 @@ export class ERC20TransferComponent
         ],
       ],
       value: ['', [Validators.required, Validators.min(1)]],
+    });
+
+    // EVENTOS
+    this._ethersjsService.getSignerSubject().subscribe(async (_signer) => {
+      //Se a conta não for nula, cria uma nova subscrição filtrando por eventos `Transfer`
+      // que tenha a conta `from` igual à conta conectada na Wallet
+      if (_signer) {
+        // subscrição eventos últimos 1000 blocos
+        this.fetchPastTransferEvents(_signer);
+        // subscrição eventos futuros
+        await this.contractERC20.subscribeContractEvent({
+          eventName: 'Transfer',
+          args: [await _signer.getAddress()],
+          listenerFunction: (from, to, value, event) => {
+            this.eventList = [
+              ...this.eventList,
+              {
+                blockNumber: event.blockNumber,
+                from,
+                to,
+                value,
+              },
+            ];
+          },
+        });
+      }
     });
   }
 
@@ -81,5 +113,66 @@ export class ERC20TransferComponent
         `The data filled in the form is not valid. Please, fill the form correctly before submit it`
       );
     }
+  }
+
+  /**
+   * Fetches the past events on the blockchain since current block less 1000 and feed the {pastEvents} array
+   *
+   * @param signer Account address used to filter the events where 'from' part equals it
+   */
+  private async fetchPastTransferEvents(_signer: Signer): Promise<void> {
+    const currentBlock = await this._ethersjsService.getCurrentBlockNumber();
+    // subscrição eventos passados
+    const pastEvents = await this.contractERC20.getContractsPastEvent({
+      eventName: 'Transfer',
+      args: [],
+      fromBlock: currentBlock - 1000,
+      toBlock: 'latest',
+    });
+
+    ////////////////////////////////
+
+    // const linkContract = new ethers.Contract(
+    //   '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
+    //   LINK_TOKEN_ABI,
+    //   new ethers.providers.Web3Provider(window.ethereum)
+    // );
+    // const filterTransfer = linkContract.filters['Transfer']();
+    // const transferEvents = await linkContract.queryFilter(
+    //   filterTransfer,
+    //   currentBlock - 50,
+    //   'latest'
+    // );
+    // console.log(
+    //   'Transfers____',
+    //   transferEvents.map((e) => {
+    //     return { transaction: e.transactionHash, block: e.blockNumber };
+    //   }),
+    //   transferEvents
+    // );
+
+    // console.warn(
+    //   '#DEBUG',
+    //   transferEvents.length,
+    //   currentBlock,
+    //   transferEvents[transferEvents.length - 1],
+    //   transferEvents[0],
+    //   //      33084061, // BLOCK WHERE IS THE TRANSACTION IN KOVAN
+    //   11138995, // BLOCK WHERE IS THE TRANSACTION IN RINKEBY
+    //   pastEvents.filter((e) => {
+    //     return e.transactionHash.startsWith('0x55c2dd6b760a');
+    //   })
+    // );
+
+    const tempArray = [];
+    for (const e of pastEvents) {
+      tempArray.push({
+        blockNumber: e.blockNumber,
+        from: (<Result>e.args)['from'],
+        to: (<Result>e.args)['to'],
+        value: (<Result>e.args)['value'],
+      });
+    }
+    this.eventList = [...this.eventList, ...tempArray];
   }
 }
