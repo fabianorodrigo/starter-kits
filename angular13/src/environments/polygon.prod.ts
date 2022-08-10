521caa7a7a76b398300516eb137dd97a2927138b
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-08-10 16:01:07 -0300

    angular13.web3ui: Adequação das environments de produção ao novo formato definido para desenvolvimento

--- angular13/src/environments/polygon.prod.ts
+++ angular13/src/environments/polygon.prod.ts
@@ -5,3 +5,5 @@ export const POLYGON_LINK_TOKEN = '0xb0897686c545045afc77cf20ec7a532e3120e0f1';
 // https://docs.chain.link/docs/matic-addresses/#Mumbai%20Testnet
 export const POLYGON_DATA_FEED_ETH_USD_ADDRESS =
   '0xF9680D99D6C9589e2a93a78A04A279e509205945';
+
+export const POLYGON_CHAIN_BATTLES_NFT_ADDRESS = '';
